70d2ee9767ecf606a54cbb413f7ce033d84d2afe
milinsky <mail@milinsky.ru>
2019-12-19 19:56:41 +1000

    add ValidateFieldAction execute

--- src/Actions/ValidateFieldAction.php
+++ src/Actions/ValidateFieldAction.php
@@ -5,8 +5,25 @@ declare(strict_types=1);
 namespace Marussia\ContentField\Actions;
 
 use Marussia\ContentField\FieldValue;
+use Marussia\ContentField\FieldHandlerCollector;
 
 class ValidateFieldAction
 {
+    private $handlerCollector;
     
+    public function __construct(FieldHandlerCollector $handlerCollector)
+    {
+        $this->handlerCollector = $handlerCollector;
+    }
+
+    public function execute() : FieldValue
+    {
+        
+    }
+
+    public function data(array $data) : self
+    {
+        $this->data = $data;
+        return $this;
+    }
 }
